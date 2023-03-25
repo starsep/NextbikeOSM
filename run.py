acf0d79cc36d71a700cc283ac6a84ac706fa67d3
Filip Czaplicki <github@starsep.com>
2023-03-25 10:41:22 +0100

    Sort and simplify list of networks/cities

--- run.py
+++ run.py
@@ -11,35 +11,14 @@ if __name__ == "__main__":
     templatesDirectory = Path("templates")
     outputDirectory = Path("output")
     outputDirectory.mkdir(exist_ok=True)
-    networksPoland = [
-        (148, "Wrocław"),
-        (251, "Lublin"),
-        (331, "Świdnik"),
-        (255, "Grodzisk Mazowiecki"),
-        (422, "Kołobrzeg"),
-        (461, "Piaseczno"),
-        (496, "Koszalin"),
-        (504, "Pobiedziska"),
-        (518, "Piotrków Trybunalski"),
-        (545, "Konin"),
-        (529, "Zielona Góra"),
-        (548, "Tarnów"),
-        (562, "Koluszki"),
-        (563, "Łask"),
-        (564, "Łowicz"),
-        (565, "Pabianice"),
-        (566, "Sieradz"),
-        (567, "Skierniewice"),
-        (568, "Zduńska Wola"),
-        (569, "Zgierz"),
-        (570, "Kutno"),
-        (571, "Łódź"),
-        (650, "Olesno"),
-        (727, "Sokołów Podlaski"),
-        (812, "Warszawa"),
-        (831, "Wolsztyn"),
-    ]
     nextbikeParser = NextbikeParser()
+
+    networksPoland = []
+    for country in nextbikeParser.countries:
+        if country.countryCode == "PL":
+            for city in country.cities:
+                if len(city.places) > 0:
+                    networksPoland.append((city.uid, city.name.removesuffix(" (RL)")))
     for networkId, cityName in networksPoland:
         slug = slugify(cityName)
         main(
@@ -54,7 +33,7 @@ if __name__ == "__main__":
 
     environment = Environment(loader=PackageLoader("nextbike_valid", "templates"))
     template = environment.get_template("index.html")
-    cities = [(cityName, slugify(cityName)) for (_, cityName) in networksPoland]
+    cities = sorted([(cityName, slugify(cityName)) for (_, cityName) in networksPoland], key=lambda x: x[0])
     with (outputDirectory / "index.html").open("w", encoding="utf-8") as f:
         f.write(template.render(dict(cities=cities)))
     shutil.copy(templatesDirectory / "index.js", outputDirectory / "index.js")
